27a9d093a569ed0338f6fde45ee0ada4f7a78cdd
Ousainu Jabbi <ousainujabbi@gmail.com>
2020-07-03 18:15:07 -0400

    React with D3

--- server/db/db.js
+++ server/db/db.js
@@ -11,6 +11,16 @@ const db = new Sequelize(
 )
 module.exports = db
 
+// const mongoose = require('mongoose')
+// const pkg = require('../../package.json')
+
+// const databaseName = pkg.name + (process.env.NODE_ENV === 'test' ? '-test' : '')
+
+// const db = new mongoose(
+//   process.env.DATABASE_URL || `mongodb://localhost:27017/${databaseName}`
+// )
+// module.exports = db
+
 // This is a global Mocha hook used for resource cleanup.
 // Otherwise, Mocha v4+ does not exit after tests.
 if (process.env.NODE_ENV === 'test') {
