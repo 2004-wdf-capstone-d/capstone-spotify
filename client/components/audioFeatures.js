e97da11a71f7adf65a1cd8727d9c102894d34568
Ube Halaya <eli.tamondong@gmail.com>
2020-07-11 17:41:52 -0400

    hide sidebar when not logged in

--- client/components/audioFeatures.js
+++ client/components/audioFeatures.js
@@ -27,68 +27,63 @@ export const AudioFeatures = props => {
     .domain(currentAudioFeature.map(dataPoint => dataPoint.trackName))
     .range([0, 50 * currentAudioFeature.length])
 
-  if (currentAudioFeature.length) {
-    return (
-      <div>
-        <h1>Audio Features of the Top Streaming Tracks on Spotify</h1>
-        <h5>based on global weekly charts from July 9, 2020</h5>
-        <div id="audio-feature-graph">
-          <h3>Audio Feature: {currentAudioFeature[0].feature}</h3>
-          <label htmlFor="change-feature">Change Feature:</label>
-          <select
-            name="change-feature"
-            id="change-audio-feature"
-            onChange={event => {
-              handleChange(event)
-            }}
-          >
-            <option value="danceability">Danceability</option>
-            <option value="energy">Energy</option>
-            <option value="speechiness">Speechiness</option>
-            <option value="acousticness">Acousticness</option>
-            <option value="liveness">Liveness</option>
-            <option value="valence">Valence</option>
-          </select>
-          <label htmlFor="af-value-sorter">Sort by:</label>
-          <select
-            name="sorters"
-            id="af-value-sorter"
-            onChange={event => {
-              handleSort(event)
-            }}
-          >
-            <option value="position">Chart Ranking</option>
-            <option value="descending">High to Low</option>
-            <option value="ascending">Low to High</option>
-          </select>
-          <svg
-            width={width}
-            height={y.range()[1]}
-            fontFamily="sans-serif"
-            fontSize="18"
-          >
-            {currentAudioFeature.map((dataPoint, index) => (
-              <g
-                key={index}
-                transform={`translate(0,${y(dataPoint.trackName)})`}
-              >
-                <rect
-                  fill="limegreen"
-                  width={x(dataPoint.value)}
-                  height={y.bandwidth() - 1}
-                />
-                <text fill="black" x={x(0)} y={y.bandwidth() / 2} dy="0.35em">
-                  {dataPoint.artist} : {dataPoint.trackName}
-                </text>
-              </g>
-            ))}
-          </svg>
-        </div>
+  return currentAudioFeature.length ? (
+    <div>
+      <h1>Audio Features of the Top Streaming Tracks on Spotify</h1>
+      <h5>based on global weekly charts from July 9, 2020</h5>
+      <div id="audio-feature-graph">
+        <h3>Audio Feature: {currentAudioFeature[0].feature}</h3>
+        <label htmlFor="change-feature">Change Feature:</label>
+        <select
+          name="change-feature"
+          id="change-audio-feature"
+          onChange={event => {
+            handleChange(event)
+          }}
+        >
+          <option value="danceability">Danceability</option>
+          <option value="energy">Energy</option>
+          <option value="speechiness">Speechiness</option>
+          <option value="acousticness">Acousticness</option>
+          <option value="liveness">Liveness</option>
+          <option value="valence">Valence</option>
+        </select>
+        <label htmlFor="af-value-sorter">Sort by:</label>
+        <select
+          name="sorters"
+          id="af-value-sorter"
+          onChange={event => {
+            handleSort(event)
+          }}
+        >
+          <option value="position">Chart Ranking</option>
+          <option value="descending">High to Low</option>
+          <option value="ascending">Low to High</option>
+        </select>
+        <svg
+          width={width}
+          height={y.range()[1]}
+          fontFamily="sans-serif"
+          fontSize="18"
+        >
+          {currentAudioFeature.map((dataPoint, index) => (
+            <g key={index} transform={`translate(0,${y(dataPoint.trackName)})`}>
+              <rect
+                fill="limegreen"
+                width={x(dataPoint.value)}
+                height={y.bandwidth() - 1}
+              />
+              <text fill="black" x={x(0)} y={y.bandwidth() / 2} dy="0.35em">
+                {dataPoint.artist} : {dataPoint.trackName}
+              </text>
+            </g>
+          ))}
+        </svg>
       </div>
-    )
-  } else {
-    return <h5>Loading...</h5>
-  }
+    </div>
+  ) : (
+    <h5>Loading...</h5>
+  )
 }
 
 const mapState = state => {
--- client/components/sidebar.js
+++ client/components/sidebar.js
@@ -3,11 +3,22 @@ import {connect} from 'react-redux'
 
 import {Link} from 'react-router-dom'
 
-const Sidebar = props => (
-  <div id="sidebar">
-    <Link to="/top-ten-global">Top Streaming Artists</Link>
-    <Link to="/"> user's top artists</Link>
-  </div>
-)
+const Sidebar = props => {
+  const {isLoggedIn} = props
+  return isLoggedIn ? (
+    <div id="sidebar">
+      <Link to="/top-ten-global">Top Streaming Artists</Link>
+      <Link to="/"> user's top artists</Link>
+    </div>
+  ) : (
+    <div />
+  )
+}
 
-export default connect(null)(Sidebar)
+const mapState = state => {
+  return {
+    isLoggedIn: !!state.user._id
+  }
+}
+
+export default connect(mapState)(Sidebar)
