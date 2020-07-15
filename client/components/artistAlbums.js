95dff7bb1ec314640288cb223022ed7fd3ba237f
Ousainu Jabbi <ousainujabbi@gmail.com>
2020-07-15 11:52:01 -0400

    work in progress

--- client/components/artistAlbums.js
+++ client/components/artistAlbums.js
@@ -6,81 +6,94 @@ const artistAlbums = props => {
   // let data = props.singleTopArtist.albums
 
   let data = {
-    name: 'flare',
-    albums: [
+    name: 'TOPICS',
+    children: [
       {
-        albumName: 'Hi!',
-        tracks: [
+        name: 'Topic A',
+        children: [{name: 'Sub A1', size: 4}, {name: 'Sub A2', size: 4}]
+      },
+      {
+        name: 'Topic B',
+        children: [
+          {name: 'Sub B1', size: 3},
+          {name: 'Sub B2', size: 3},
           {
-            trackName: 'track1'
-          },
-          {
-            trackName: 'track2'
+            name: 'Sub B3',
+            size: 3
           }
         ]
       },
       {
-        albumName: 'Bye!',
-        tracks: [
-          {
-            trackName: 'track3'
-          },
-          {
-            trackName: 'track4'
-          }
-        ]
+        name: 'Topic C',
+        children: [{name: 'Sub A1', size: 4}, {name: 'Sub A2', size: 4}]
       }
     ]
   }
 
-  const partition = someData => {
-    const root = d3
-      .hierarchy(someData)
-      .sum(d => d.value)
-      .sort((a, b) => b.value - a.value)
-    return d3.partition().size([2 * Math.PI, root.height + 1])(root)
-  }
+  let width = 500 // <-- 1
+  let height = 500
+  let radius = Math.min(width, height) / 2 // < -- 2
+  let color = d3.scaleOrdinal(d3.schemeCategory10)
 
-  const root = partition(data)
+  const partition = d3.partition().size([2 * Math.PI, radius])
+
+  const root = d3.hierarchy(data).sum(d => d.size)
   console.log('ROOT', root)
 
-  let color = d3.scaleOrdinal(
-    d3.quantize(d3.interpolateRainbow, data.albums.length)
-  )
-
-  let format = d3.format(',d')
-
-  let width = 932
-
-  let radius = width / 6
+  console.log('PARTITION(ROOT)', partition(root))
 
   const arc = d3
     .arc()
     .startAngle(d => d.x0)
     .endAngle(d => d.x1)
-    .padAngle(d => Math.min((d.x1 - d.x0) / 2, 0.005))
-    .padRadius(radius * 1.5)
-    .innerRadius(d => d.y0 * radius)
-    .outerRadius(d => Math.max(d.y0 * radius, d.y1 * radius - 1))
+    .innerRadius(d => d.y0)
+    .outerRadius(d => d.y1)
+
+  // const partition = someData => {
+  //   const root = d3
+  //     .hierarchy(someData)
+  //     .sum(d => d.value)
+  //     .sort((a, b) => b.value - a.value)
+  //   return d3.partition().size([2 * Math.PI, root.height + 1])(root)
+  // }
+
+  // const root = partition(data)
+
+  // console.log('ROOT', root)
+
+  // let color = d3.scaleOrdinal(
+  //   d3.quantize(d3.interpolateRainbow, data.albums.length)
+  // )
+
+  // let format = d3.format(',d')
+
+  // let width = 932
+
+  // let radius = width / 6
+
+  // const arc = d3
+  //   .arc()
+  //   .startAngle(d => d.x0)
+  //   .endAngle(d => d.x1)
+  //   .padAngle(d => Math.min((d.x1 - d.x0) / 2, 0.005))
+  //   .padRadius(radius * 1.5)
+  //   .innerRadius(d => d.y0 * radius)
+  //   .outerRadius(d => Math.max(d.y0 * radius, d.y1 * radius - 1))
 
   return (
     <div>
-      <svg
-        viewBox={`0 0 ${width} ${width}`}
-        fontFamily="sans-serif"
-        fontSize="10"
-      >
+      <svg width={width} height={height}>
         <g transform={`translate(${width / 2}, ${width / 2})`}>
-          <path
-            func={d3
-              .selectAll('path')
-              .data(root.descendants().slice(1))
-              .join('path')
-              .attr('fill', d => {
-                while (d.depth > 1) d = d.parent
-                return color(d.data.name)
-              })}
-          />
+          {root.children.map((child, index) => (
+            <path
+              key={index}
+              data={partition(child)}
+              display={child.depth ? null : 'none'}
+              d={arc}
+              stroke="#fff"
+              fill={d => color(d.children ? d : d.parent).data.name}
+            />
+          ))}
         </g>
       </svg>
     </div>
@@ -99,3 +112,30 @@ const mapState = state => ({
 })
 
 export default connect(mapState, null)(artistAlbums)
+
+// return (
+//   <div>
+//     <svg
+//       // viewBox={`0 0 ${width} ${width}`}
+//       // fontFamily="sans-serif"
+//       // fontSize="10"
+
+//     >
+//       {/* <g transform={`translate(${width / 2}, ${width / 2})`}>
+//         {data.albums.map((album) => (
+//           <g key={album.id}>
+//             <path
+//               fill={(d) => {
+//                 while (d.depth > 1) d = d.parent
+//                 return color(d.data.name)
+//               }}
+//               fillOpacity={(d) =>
+//                 arcVisible(d.current) ? (d.albums.children ? 0.6 : 0.4) : 0
+//               }
+//               d={(d) => arc(d.current)}
+//             ></path>
+//           </g>
+//         ))}
+//       </g> */}
+//     </svg>
+//   </div>
